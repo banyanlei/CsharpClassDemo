636e4afe96227c90a0ffcf33044b28368db17049
banyanlei <1428609906@qq.com>
2023-10-08 14:04:37 +0800

    add BilliWindowsForms

---
+++ BilliWindowsForms/BilliWindowsForms/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace BilliWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonLast = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonOpenForm2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(73, 33);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(131, 40);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "打开文件夹";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.ButtonOpen_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(73, 131);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(302, 444);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // buttonLast
+            // 
+            this.buttonLast.Location = new System.Drawing.Point(478, 198);
+            this.buttonLast.Name = "buttonLast";
+            this.buttonLast.Size = new System.Drawing.Size(114, 67);
+            this.buttonLast.TabIndex = 2;
+            this.buttonLast.Text = "上一首";
+            this.buttonLast.UseVisualStyleBackColor = true;
+            this.buttonLast.Click += new System.EventHandler(this.ButonLast_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(478, 293);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(114, 67);
+            this.buttonNext.TabIndex = 3;
+            this.buttonNext.Text = "下一首";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.ButtonNext_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(882, 519);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(95, 40);
+            this.buttonClose.TabIndex = 4;
+            this.buttonClose.Text = "关闭";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // buttonOpenForm2
+            // 
+            this.buttonOpenForm2.Location = new System.Drawing.Point(882, 429);
+            this.buttonOpenForm2.Name = "buttonOpenForm2";
+            this.buttonOpenForm2.Size = new System.Drawing.Size(95, 42);
+            this.buttonOpenForm2.TabIndex = 5;
+            this.buttonOpenForm2.Text = "打开form2";
+            this.buttonOpenForm2.UseVisualStyleBackColor = true;
+            this.buttonOpenForm2.Click += new System.EventHandler(this.ButtonOpenForm2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 627);
+            this.Controls.Add(this.buttonOpenForm2);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonLast);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonLast;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonOpenForm2;
+    }
+}
+
---
+++ BilliWindowsForms/BilliWindowsForms/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BilliWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> strNames = new List<string>();
+        List<string> musicfiles = new List<string>();
+
+
+        private void ButtonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择音乐";
+            ofd.InitialDirectory = @"C:\CAYA\cy\c# test\BilliWindowsForms\BilliWindowsForms\music";
+            ofd.Filter = "音乐文件(*.wav)|*.wav|全部文件(*.*)|*.*";
+            ofd.Multiselect = true;
+            ofd.ShowDialog();
+            strNames.AddRange(ofd.SafeFileNames);
+            musicfiles.AddRange(ofd.FileNames);
+            //将文件名添加到 listbox 中
+            for (int i = 0; i < strNames.Count; i++)
+            {
+                listBox1.Items.Add(strNames[i]);
+            }
+            buttonLast.Visible = true;
+            buttonNext.Visible = true;
+        }
+
+            SoundPlayer sp = new SoundPlayer();
+        public void SoundPlay(int index)
+        {
+            sp.SoundLocation = musicfiles[index];
+            sp.Play();
+        }
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SoundPlay(listBox1.SelectedIndex);
+        }
+        int NowSelected = 0;
+        private void ButonLast_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                NowSelected = listBox1.SelectedIndex;
+            }
+            else
+            {
+                NowSelected = 1;
+            }
+            NowSelected--;
+            if (NowSelected < 0)
+            {
+                NowSelected = musicfiles.Count - 1;
+            }
+            listBox1.SelectedIndex = NowSelected;
+            SoundPlay(NowSelected);
+        }
+
+        private void ButtonNext_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                NowSelected = listBox1.SelectedIndex;
+            }
+            else
+            {
+                NowSelected = musicfiles.Count-1;
+            }
+            NowSelected++;
+            if (NowSelected == musicfiles.Count)
+            {
+                NowSelected = 0;
+            }
+            listBox1.SelectedIndex = NowSelected;
+            SoundPlay(NowSelected);
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonOpenForm2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Test.form = this;
+            buttonLast.Visible = false;
+            buttonNext.Visible = false;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
